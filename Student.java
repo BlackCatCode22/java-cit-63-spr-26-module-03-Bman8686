18a60bc0215794143c1435633449cb45daa9ee87
Bman8686 <110802592+Bman8686@users.noreply.github.com>
2026-02-22 19:24:01 -0800

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,18 @@
+package mystudent;
+
+import java.util.Scanner;
+
+public class Student {
+
+    String firstName;
+    String lastName;
+    double gpa;
+    String major;
+    int age;
+    boolean onProbation;
+
+
+}
+
+
+
