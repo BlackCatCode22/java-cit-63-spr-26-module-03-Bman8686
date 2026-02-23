d825afde195bd2cd4a7e70c630f3e6f3a4e44208
Bman8686 <110802592+Bman8686@users.noreply.github.com>
2026-02-22 22:58:16 -0800

    Add files via upload

---
+++ Dog.java
@@ -0,0 +1,33 @@
+package myanimals;
+
+public class Dog {
+
+    private static int dogCount = 0;
+
+    String name;
+
+    int age;
+
+    int livesRemaining;
+
+    public void woof() {
+
+        System.out.println("Woof");
+
+       public Dog() {
+            dogCount++;
+        }
+        public static int getDogCount(){
+
+            return dogCount;
+
+
+
+
+        }
+
+    }
+}
+
+
+
