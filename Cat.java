ab50082ad7d1bfcddd0d72b644538a046d22cca5
Bman8686 <110802592+Bman8686@users.noreply.github.com>
2026-02-22 22:57:47 -0800

    Add files via upload

---
+++ Cat.java
@@ -0,0 +1,30 @@
+package myanimals;
+
+public class Cat {
+    private static int catCount = 0;
+
+    String name;
+
+    int age;
+
+    int livesRemaining;
+
+    public void meow() {
+
+        System.out.println("Meow");
+
+       public Cat() {
+            catCount++;
+        }
+        public static int getCatCount(){
+
+           return catCount;
+
+
+
+
+        }
+
+    }
+}
+
