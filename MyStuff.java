d06050e235a9ddd6fc97d8fb0b2fe296007c9079
Bman8686 <110802592+Bman8686@users.noreply.github.com>
2026-02-22 19:26:03 -0800

    Add files via upload

---
+++ MyStuff.java
@@ -0,0 +1,18 @@
+package mystuff;
+
+public class MyStuff {
+
+
+        String myTruck;
+        String myModel;
+        int myYear ;
+        String myColor;
+        boolean fullLoad;
+
+
+    }
+
+
+
+
+
