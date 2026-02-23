cc848a18d6b4411c74239a655f2bfc074d403c80
Bman8686 <110802592+Bman8686@users.noreply.github.com>
2026-02-22 22:57:12 -0800

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,23 @@
+package myanimals;
+
+public class Animal {
+
+    public static void main(String[] args) {
+    System.out.println(Cat.getCatCount());
+
+    Cat myCat = new Cat();
+
+    myCat.meow();
+    myDog.woof();
+    myCat.name = "Kitty";
+    myDog.name = "Fido"
+    myCat.age =10;
+    myDog.age =7;
+
+    System.out.println(Cat.getCatCount());
+    System.out.println(Dog.getDogCount());
+
+}
+
+}
+
